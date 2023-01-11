7443de7eb28a151f8073b09b75686320daa6bd14
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-11 16:00:32 -0300

    chore(Weather): add the OpenWeather configs

---
+++ src/features/weather/configs/openWeatherMapConfig.ts
@@ -0,0 +1,8 @@
+import { OPEN_WEATHER_MAP_API_KEY } from "@env";
+
+const openWeatherMapConfig = {
+  key: OPEN_WEATHER_MAP_API_KEY,
+  url: "https://api.openweathermap.org/data/2.5/weather",
+};
+
+export default openWeatherMapConfig;
