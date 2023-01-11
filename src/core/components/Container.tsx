fb7fd09e6f681c7fb282807f003286a73a0d316a
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-11 08:48:25 -0300

    feat(Container): create the Container component

---
+++ src/core/components/Container.tsx
@@ -0,0 +1,7 @@
+import Styled from "styled-components/native";
+
+const Container = Styled.View`
+  flex: 1;
+`;
+
+export default Container;
--- src/core/providers/AppProvider.tsx
+++ src/core/providers/AppProvider.tsx
@@ -1,29 +1,21 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
 import { ThemeProvider } from "@rneui/themed";
 
 import theme from "core/configs/theme";
+import Container from "core/components/Container";
+
 import MapScreen from "screens/MapScreen";
 
 function AppProvider() {
   return (
     <ThemeProvider theme={theme}>
-      <View style={styles.container}>
+      <Container>
         <MapScreen />
         <StatusBar style="auto" />
-      </View>
+      </Container>
     </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default AppProvider;
