f4b100b574941c4ac0fa0637ebc3274d45394791
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-11 08:30:19 -0300

    chore(babel.config): configure the alias

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            core: "./src/core",
+            features: "./src/features",
+            screens: "./src/screens",
+          },
+        },
+      ],
+    ],
   };
 };
