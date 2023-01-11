52be01b3d7bd5fd7026d2f5a27e17d7ad9c868eb
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-11 16:03:25 -0300

    feat(MapScreen): add the WeatherBottomSheet component in the screen

--- src/features/maps/components/Map/index.tsx
+++ src/features/maps/components/Map/index.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { MapViewProps } from "react-native-maps";
-
-import LoadingScreen from "core/screens/LoadingScreen";
-
-import useLocation from "features/locations/hooks/useLocation";
+import { LocationObject } from "expo-location";
 
 import { MapView } from "./Map.styles";
 
-function Map(props: MapViewProps) {
-  const { location, loading, requestCurrentPosition } = useLocation();
+interface MapProps extends MapViewProps {
+  location: LocationObject;
+}
 
-  React.useEffect(() => {
-    requestCurrentPosition();
-  }, [requestCurrentPosition]);
-
-  if (loading || location === null) {
-    return <LoadingScreen />;
-  }
+function Map(props: MapProps) {
+  const { location } = props;
 
   return (
     <MapView
--- src/features/maps/screens/MapScreen/index.tsx
+++ src/features/maps/screens/MapScreen/index.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 
+import LoadingScreen from "core/screens/LoadingScreen";
 import Container from "core/components/Container";
 
 import Map from "features/maps/components/Map";
+import useLocation from "features/locations/hooks/useLocation";
+import WeatherBottomSheet from "features/weather/components/WeatherBottomSheet";
 
 function MapScreen() {
+  const { location, loading, requestCurrentPosition } = useLocation();
+
+  React.useEffect(() => {
+    requestCurrentPosition();
+  }, [requestCurrentPosition]);
+
+  if (loading || location === null) {
+    return <LoadingScreen />;
+  }
+
   return (
     <Container>
-      <Map />
+      <Map location={location} />
+      <WeatherBottomSheet location={location} />
     </Container>
   );
 }
