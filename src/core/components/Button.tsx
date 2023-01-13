a0e5e1d27625f3db7c2c7dc791c96dfbc91431fb
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-13 14:36:36 -0300

    feat(Button): update styles

--- src/core/components/Button.tsx
+++ src/core/components/Button.tsx
@@ -11,13 +11,17 @@ function Button(props: ButtonProps) {
   const { theme } = useTheme();
 
   const buttonStyle = {
-    backgroundColor: theme.colors.primary,
-    paddingVertical: 16,
+    backgroundColor:
+      props.type === "outline" ? "rgba(0,0,0,0)" : theme.colors.primary,
     borderRadius: 16,
+    borderColor: theme.colors.secondary,
+    borderWidth: props.type === "outline" ? 0.8 : 0,
   };
 
   const titleStyle = {
     fontFamily: "Poppins-SemiBold",
+    color:
+      props.type === "outline" ? theme.colors.secondary : theme.colors.black,
   };
 
   return (
