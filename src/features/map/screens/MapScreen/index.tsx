a6d6ef4aebd9e04d5164d9a4c268bf889ee87c61
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-13 14:16:19 -0300

    feat(MapScreen): update the methods of the hooks

--- src/features/map/screens/MapScreen/index.tsx
+++ src/features/map/screens/MapScreen/index.tsx
@@ -1,29 +1,23 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 
-import LoadingScreen from "core/screens/LoadingScreen";
 import Container from "core/components/Container";
 
 import Map from "features/map/components/Map";
-import useCurrentPosition from "features/location/hooks/useCurrentPosition";
 import WeatherBottomSheet from "features/weather/components/WeatherBottomSheet";
+import { useLocationStore } from "features/location/store";
 
 const MapScreen = observer(() => {
-  const { currentPositionStore, requestCurrentPosition } = useCurrentPosition();
-  const { coords, loading } = currentPositionStore;
+  const { currentPositionStore } = useLocationStore();
 
   React.useEffect(() => {
-    requestCurrentPosition();
-  }, [requestCurrentPosition]);
-
-  if (loading || coords === null) {
-    return <LoadingScreen />;
-  }
+    currentPositionStore.refresh();
+  }, []);
 
   return (
     <Container>
-      <Map coords={coords} />
-      <WeatherBottomSheet coords={coords} />
+      <Map coords={currentPositionStore.coords} />
+      <WeatherBottomSheet />
     </Container>
   );
 });
