b2ebe8acf6d7c57dfc4557ba511c326d9fe095d4
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-13 09:54:32 -0300

    feat(AppProvider): add the GestureHandlerRootView

--- src/core/providers/AppProvider.tsx
+++ src/core/providers/AppProvider.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "@rneui/themed";
 
-import FontsProvider from "core/providers/FontsProvider";
 import theme from "core/configs/theme";
+import FontsProvider from "core/providers/FontsProvider";
 
 interface AppProviderProps {
   children: React.ReactNode;
@@ -11,12 +13,20 @@ interface AppProviderProps {
 
 function AppProvider({ children }: AppProviderProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <FontsProvider>
-        <NavigationContainer>{children}</NavigationContainer>
-      </FontsProvider>
-    </ThemeProvider>
+    <GestureHandlerRootView style={styles.container}>
+      <ThemeProvider theme={theme}>
+        <FontsProvider>
+          <NavigationContainer>{children}</NavigationContainer>
+        </FontsProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default AppProvider;
