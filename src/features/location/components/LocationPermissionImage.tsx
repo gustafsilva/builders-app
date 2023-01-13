26507d3c36c1c29947205a92811da67b9048a202
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-13 12:15:33 -0300

    fix(LocationPermissionImage): update imports

--- src/features/location/components/LocationPermissionImage.tsx
+++ src/features/location/components/LocationPermissionImage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { SvgXml } from "react-native-svg";
 
-import locationPermissionImage from "features/locations/assets/imgs/locationPermissionImage.svg";
+import locationPermissionImage from "features/location/assets/imgs/locationPermissionImage.svg";
 
 function LocationPermissionImage() {
   return <SvgXml width="321px" height="445px" xml={locationPermissionImage} />;
