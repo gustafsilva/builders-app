7187de176bd2de2550398b29fd1c051bc9ff99bd
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-17 19:28:40 -0300

    feat(getWeatherForecast): create the function

---
+++ src/features/weather/services/getWeatherForecast.ts
@@ -0,0 +1,35 @@
+import Coords from "core/types/coords";
+import { handleGenericError } from "core/helpers/errors";
+
+import WeatherForest from "features/weather/types/WeatherForest";
+import openWeatherMapConfig from "features/weather/configs/openWeatherMapConfig";
+
+async function getWeatherForecast(
+  coords: Coords
+): Promise<WeatherForest[] | null> {
+  const weatherForecastUrl = `${openWeatherMapConfig.url}forecast?lat=${coords.latitude}&lon=${coords.longitude}&appid=${openWeatherMapConfig.key}&lang=pt_br&units=metric`;
+
+  return (
+    fetch(weatherForecastUrl)
+      .then((response) => response.json())
+      .then((json) => json.list)
+      .then((list) =>
+        list.map(
+          (forecast: any) =>
+            ({
+              id: Number(forecast.weather[0].id),
+              date: new Date(forecast.dt_txt),
+              temp: Number(forecast.main.temp),
+              tempMin: Number(forecast.main.temp_min),
+              tempMax: Number(forecast.main.temp_max),
+            } as WeatherForest)
+        )
+      )
+      .catch((error) => {
+        handleGenericError(error);
+        return null;
+      })
+  );
+}
+
+export default getWeatherForecast;
