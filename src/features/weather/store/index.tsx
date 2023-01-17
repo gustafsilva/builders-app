f19b248ec001a232e73b8d440fd60f1ff808afb4
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-17 19:30:07 -0300

    chore(weatherStore): add the weatherForecastStore

--- src/features/weather/store/index.tsx
+++ src/features/weather/store/index.tsx
@@ -1,9 +1,11 @@
 import { createContext, useContext } from "react";
 
 import WeatherResumeState from "features/weather/store/states/WeatherResumeState";
+import WeatherForecastState from "./states/WeatherForecastState";
 
 const weatherStore = {
   weatherResumeStore: new WeatherResumeState(),
+  weatherForecastStore: new WeatherForecastState(),
 };
 
 export const WeatherStoreContext = createContext(weatherStore);
