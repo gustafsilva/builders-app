afb8da5c72e946c8b5d32bca67eecf8ee2c39d79
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-17 10:44:56 -0300

    feat(spacing): create calculateSpacingInNumber function

--- src/core/helpers/spacing.ts
+++ src/core/helpers/spacing.ts
@@ -1,11 +1,25 @@
-export type Spacing = number | "view" | "card";
+export type SpacingInNumber = number | undefined;
 
-export function calculateSpacing(spacing: Spacing | undefined) {
+export type Spacing = SpacingInNumber | "view" | "card" | string;
+
+export function calculateSpacingInNumber(spacing: Spacing) {
   if (spacing === "view") {
     return 24;
   }
   if (spacing === "card") {
     return 16;
+  }
+  if (typeof spacing === "number") {
+    return spacing;
+  }
+
+  return undefined;
+}
+
+export function calculateSpacing(spacing: Spacing) {
+  const spacingInNumber = calculateSpacingInNumber(spacing);
+  if (spacingInNumber !== undefined) {
+    return spacingInNumber;
   }
 
   return spacing;
