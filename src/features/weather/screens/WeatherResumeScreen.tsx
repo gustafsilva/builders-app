0a1b6ce8a483d67f7889784395243aff483b5e45
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-17 23:34:09 -0300

    feat(WeatherResumeScreen): add weather forecast info in the screen

--- src/features/weather/screens/WeatherResumeScreen.tsx
+++ src/features/weather/screens/WeatherResumeScreen.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useTheme } from "@rneui/themed";
 
 import ScrollScreenLayout from "core/layouts/ScrollScreenLayout";
 import Content from "core/components/Content";
 import Box from "core/components/Box";
 import Button from "core/components/Button";
+import Text from "core/components/Text";
+import Spinner from "core/components/Spinner";
 
-import useWeatherResume from "features/weather/hooks/useWeatherResume";
+import useWeatherStats from "features/weather/hooks/useWeatherStats";
 import WeatherBasicInfo from "features/weather/components/WeatherBasicInfo";
 import WeatherResumeItem from "features/weather/components/WeatherResumeItem";
+import WeatherIcon from "features/weather/components/WeatherIcon";
 
 const WeatherResumeScreen = observer(() => {
-  const { weatherResumeStore, refresh } = useWeatherResume();
+  const {
+    currentPositionStore,
+    weatherForecastStore,
+    weatherResumeStore,
+    weatherForecastOfNextDays,
+    refreshAll,
+  } = useWeatherStats();
+  const { theme } = useTheme();
 
   const infos = React.useMemo(() => {
     if (weatherResumeStore.info === null) {
@@ -58,37 +70,129 @@ const WeatherResumeScreen = observer(() => {
     ];
   }, [weatherResumeStore.info]);
 
+  React.useEffect(() => {
+    weatherForecastStore.refresh(currentPositionStore);
+  }, []);
+
   return (
     <ScrollScreenLayout
       loading={weatherResumeStore.loading || weatherResumeStore.info === null}
       headerShown
+      flexGrow={1}
     >
-      {weatherResumeStore.info !== null && (
-        <Content>
-          <WeatherBasicInfo />
+      <Content>
+        <WeatherBasicInfo />
 
-          <Box flex={1}>
-            {infos?.map((info, key) => (
-              <Box direction="row" mt={16} key={key}>
-                <WeatherResumeItem
-                  title={info[0].title}
-                  icon={info[0].icon}
-                  description={info[0].description}
-                />
-                <WeatherResumeItem
-                  title={info[1].title}
-                  icon={info[1].icon}
-                  description={info[1].description}
+        {weatherForecastStore.loading || weatherForecastStore.info === null ? (
+          <Box flex={1} justifyContent="center" alignItems="center">
+            <Spinner />
+          </Box>
+        ) : (
+          <React.Fragment>
+            <Box
+              flex={1}
+              backgroundColor={theme.colors.primary}
+              borderRadius={8}
+              p="card"
+              mt={24}
+            >
+              <Box mb={8}>
+                <Box direction="row" pb={8}>
+                  <Box mr={8}>
+                    <MaterialCommunityIcons
+                      name="calendar"
+                      size={24}
+                      color={theme.colors.secondary}
+                    />
+                  </Box>
+                  <Text h4>Previsão para 5 dias</Text>
+                </Box>
+                <Box
+                  height={0.8}
+                  width="100%"
+                  backgroundColor={theme.colors.tertiary}
+                  opacity={0.4}
+                  mt={8}
                 />
               </Box>
-            ))}
-          </Box>
 
-          <Button onPress={refresh} size="lg">
-            Atualizar
-          </Button>
-        </Content>
-      )}
+              <Box>
+                {weatherForecastOfNextDays !== null &&
+                  weatherForecastOfNextDays.map((forecast, index) => (
+                    <Box mb={8} key={forecast.date.toString()}>
+                      <Box
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                      >
+                        <Box
+                          flex={1}
+                          direction="row"
+                          justifyContent="space-between"
+                          alignItems="center"
+                        >
+                          <Text bold>
+                            {forecast.date.toDate().toLocaleDateString()}
+                          </Text>
+                          <WeatherIcon id={forecast.id} size="small" />
+                        </Box>
+
+                        <Box
+                          flex={1}
+                          direction="row"
+                          justifyContent="flex-end"
+                          alignItems="center"
+                        >
+                          <Text>{forecast.tempMinOfDay.toFixed(0)}°C</Text>
+                          <Box
+                            height={8}
+                            borderRadius={8}
+                            width="40%"
+                            backgroundColor={theme.colors.tertiary}
+                            mh={8}
+                          />
+                          <Text>{forecast.tempMaxOfDay.toFixed(0)}°C</Text>
+                        </Box>
+                      </Box>
+
+                      {index < weatherForecastOfNextDays.length - 1 && (
+                        <Box
+                          height={0.8}
+                          width="100%"
+                          backgroundColor={theme.colors.tertiary}
+                          opacity={0.4}
+                          mt={8}
+                        />
+                      )}
+                    </Box>
+                  ))}
+              </Box>
+            </Box>
+            <Box>
+              {infos?.map((info, key) => (
+                <Box direction="row" mt={16} key={key}>
+                  <WeatherResumeItem
+                    title={info[0].title}
+                    icon={info[0].icon}
+                    description={info[0].description}
+                  />
+                  <WeatherResumeItem
+                    title={info[1].title}
+                    icon={info[1].icon}
+                    description={info[1].description}
+                  />
+                </Box>
+              ))}
+            </Box>
+
+            <Box mt={24}>
+              <Button onPress={refreshAll} size="lg">
+                Atualizar
+              </Button>
+            </Box>
+          </React.Fragment>
+        )}
+      </Content>
     </ScrollScreenLayout>
   );
 });
