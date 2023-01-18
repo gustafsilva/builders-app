3cb056069adc1d77082684cd72df3b25e7f6d8ed
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-17 21:31:47 -0300

    refactor(ScrollContainer): remove component

--- src/core/components/ScrollContainer.tsx
+++
@@ -1,9 +0,0 @@
-import Styled from "styled-components/native";
-
-const ScrollContainer = Styled.ScrollView.attrs({
-  contentContainerStyle: {
-    flexGrow: 1,
-  },
-})``;
-
-export default ScrollContainer;
