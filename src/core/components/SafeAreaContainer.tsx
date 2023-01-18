30044cdd188495f00e3b55b9d3040a845b341c0d
Gustavo Freitas <gustafsilva.dev@gmail.com>
2023-01-17 21:24:42 -0300

    feat(SafeAreaContainer): use SafeAreaView of the react-native-safe-area-context lib

--- src/core/components/SafeAreaContainer.tsx
+++ src/core/components/SafeAreaContainer.tsx
@@ -1,7 +1,18 @@
-import Styled from "styled-components/native";
+import React from "react";
+import { StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  SafeAreaViewProps,
+} from "react-native-safe-area-context";
 
-const SafeAreaContainer = Styled.SafeAreaView`
-  flex: 1;
-`;
+function SafeAreaContainer(props: SafeAreaViewProps) {
+  return <SafeAreaView {...props} style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default SafeAreaContainer;
